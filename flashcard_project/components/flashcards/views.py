628ee7fa9e89735a812aa62e3f0bc731859147a7
niklavs.raudseps <niklavs.raudseps@nordigen.com>
2022-07-27 14:20:00 +0300

    make basic template

---
+++ flashcard_project/components/flashcards/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpRequest
+from django.shortcuts import HttpResponse, render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """
+    Main index page for flashcards.
+    """
+    return render(request, template_name="index.html")
